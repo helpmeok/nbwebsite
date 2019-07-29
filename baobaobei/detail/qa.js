1eecced5187de36f001952a18a7873a5c240052f
helpmeok <1002654545@qq.com>
2019-07-29 10:23:26 +0800

    11

--- baobaobei/detail/qa.js
+++ baobaobei/detail/qa.js
@@ -17,7 +17,7 @@ $(function() {
 	$('.close').click(function() {
 		$('.download-app').hide()
 		$('.pad-bottom').css({
-			height:"1rem"
+			height: "1rem"
 		})
 	})
 	var question_id = GetQueryString('question_id');
@@ -42,100 +42,95 @@ $(function() {
 		document.querySelector("body").style.visibility = "visible"
 		document.querySelector("html").style.visibility = "visible"
 	}, 100);
-	var res = {
-		"code": 0,
-		"message": "成功",
-		"data": {
-			"answerReplayList": [],
-			"hitsCnt": 0,
-			"oauthIntro": "",
-			"userAvatar": "http://thirdqq.qlogo.cn/g?b=oidb&k=9NF4SpIC4xZHD3zncY1UhA&s=100",
-			"weight": 0,
-			"collectCnt": 0,
-			"updateTime": 0,
-			"title": "5张图啊是大实打实大撒大师的请问请问恶趣味请问请问恶趣味啊是大啊是大啊是大",
-			"type": "",
-			"userName": "T-.-Eric",
-			"userId": 2834,
-			"content": "",
-			"attachment": [{
-				"duration": 0,
-				"thumbnail": "http://cdn.boblbee.superpapa.com.cn/8D4CF775D7DA6B982115F1FA91BE557D?imageMogr2/format/jpg/size-limit/10k!",
-				"size": 197623,
-				"id": 293081,
-				"type": 1,
-				"url": "http://cdn.boblbee.superpapa.com.cn/8D4CF775D7DA6B982115F1FA91BE557D"
-			}, {
-				"duration": 0,
-				"thumbnail": "http://cdn.boblbee.superpapa.com.cn/13D07539E53BF62688E186A0CCED4822?imageMogr2/format/jpg/size-limit/10k!",
-				"size": 176588,
-				"id": 293082,
-				"type": 1,
-				"url": "http://cdn.boblbee.superpapa.com.cn/13D07539E53BF62688E186A0CCED4822"
-			}, {
-				"duration": 0,
-				"thumbnail": "http://cdn.boblbee.superpapa.com.cn/E2943247F2A17F96CC1A8308DF619441?imageMogr2/format/jpg/size-limit/10k!",
-				"size": 367696,
-				"id": 293083,
-				"type": 1,
-				"url": "http://cdn.boblbee.superpapa.com.cn/E2943247F2A17F96CC1A8308DF619441"
-			}, {
-				"duration": 0,
-				"thumbnail": "http://cdn.boblbee.superpapa.com.cn/FF391BC1FF387478BC35D50CDA868DEC?imageMogr2/format/jpg/size-limit/10k!",
-				"size": 366668,
-				"id": 293084,
-				"type": 1,
-				"url": "http://cdn.boblbee.superpapa.com.cn/FF391BC1FF387478BC35D50CDA868DEC"
-			}, {
-				"duration": 0,
-				"thumbnail": "http://cdn.boblbee.superpapa.com.cn/67DA5998651D408B34DF39AB791849C3?imageMogr2/format/jpg/size-limit/10k!",
-				"size": 317260,
-				"id": 293085,
-				"type": 1,
-				"url": "http://cdn.boblbee.superpapa.com.cn/67DA5998651D408B34DF39AB791849C3"
-			}],
-			"isAnswered": true,
-			"ctime": 1563841834919,
-			"stick": "",
-			"answerCnt": 2,
-			"id": 21
-		}
-	}
-	$('.head-portrait').attr('src', res.data.userAvatar)
-	$('.name').html(res.data.userName)
-	$('.oauth-desc').html(res.data.oauthIntro)
-	$('.question-text').html(res.data.title)
-	var imgHtml = '';
-	res.data.attachment.forEach(function(el) {
-		imgHtml += `<img src=${el.url} class='img'>`
-	})
-	console.log(imgHtml)
-	var imgsWidth = res.data.attachment.length * 1.8
-	$('.imgs-box').css({
-		width: imgsWidth + 'rem'
-	})
-	$('.question-box .imgs-box').html(imgHtml)
-	$('.answer-count').html(res.data.answerCnt+'个回答')
-	// $.ajax({
-	// 	async: false,
-	// 	type: 'get',
-	// 	url: __api + 'v1/questionReplay/getQuestionDetail',
-	// 	header: {
-	// 		// Authorization: token,
-	// 		appname: "boblbee",
-	// 		"Access-Control-Allow-Origin": "*"
-	// 	},
-	// 	data: {
-	// 		questionId: question_id
-	// 	},
-	// 	success: function(res) {
-	// 		console.log(res)
-	// 	},
-	// 	error: function(err) {
-	// 		console.log(err)
-	// 		$('#app').hide()
-	// 	}
+	// $('.head-portrait').attr('src', res.data.userAvatar)
+	// $('.name').html(res.data.userName)
+	// $('.oauth-desc').html(res.data.oauthIntro)
+	// $('.question-text').html(res.data.title)
+	// var imgHtml = '';
+	// res.data.attachment.forEach(function(el) {
+	// 	imgHtml += `<img src=${el.url} class='img'>`
 	// })
-
+	// console.log(imgHtml)
+	// var imgsWidth = res.data.attachment.length * 1.8
+	// $('.imgs-box').css({
+	// 	width: imgsWidth + 'rem'
+	// })
+	// $('.question-box .imgs-box').html(imgHtml)
+	// $('.answer-count').html(res.data.answerCnt+'个回答')
+	$.ajax({
+		async: false,
+		type: 'get',
+		url: __api + 'v1/questionReplay/getQuestionDetail',
+		header: {
+			// Authorization: token,
+			appname: "boblbee",
+			"Access-Control-Allow-Origin": "*"
+		},
+		data: {
+			questionId: question_id
+		},
+		success: function(res) {
+			console.log(res)
+			$('.head-portrait').attr('src', res.data.userAvatar)
+			$('.name').html(res.data.userName)
+			$('.oauth-desc').html(res.data.oauthIntro)
+			$('.question-text').html(res.data.title)
+			var imgHtml = '';
+			res.data.attachment.forEach(function(el) {
+				imgHtml += `<img src=${el.url} onclick="download()" class='img'>`
+			})
+			var imgsWidth = res.data.attachment.length * 1.8
+			$('.imgs-box').css({
+				width: imgsWidth + 'rem'
+			})
+			$('.question-box .imgs-box').html(imgHtml)
+			$('.answer-count').html(res.data.answerCnt + '个回答')
+		},
+		error: function(err) {
+			console.log(err)
+			$('#app').hide()
+		}
+	})
+	$.ajax({
+		async: false,
+		type: 'get',
+		url: __api + 'v1/questionReplay/getOneQuestionReplayList',
+		header: {
+			// Authorization: token,
+			appname: "boblbee",
+			"Access-Control-Allow-Origin": "*"
+		},
+		data: {
+			questionId: question_id,
+			ctime: parseInt(Date.now()),
+			offset: 0,
+			total: 20
+		},
+		success: function(res) {
+			console.log(res)
+			var commentHtml = '';
+			res.data.forEach(function(el) {
+				commentHtml += `<div class="comment-box" onclick="download()">
+				<div class="flex" style="margin-bottom: .2rem;">
+					<img src="${el.userAvatar}" class="head-portrait">
+					<div>
+						<div class="name bold">${el.userName}</div>
+						<div class="oauth-desc">${el.oauthIntro}</div>
+					</div>
+				</div>
+				<div>
+					<div class="sigle-line-text-2">
+						${el.content}
+					</div>
+				</div>
+			</div>`
+			})
+						$('.comments').html(commentHtml)
+		},
+		error: function(err) {
+			console.log(err)
+			$('#app').hide()
+		}
+	})
 
 })
